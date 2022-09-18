6349fd41f7e0dc253cf0335142bca2d3dc3521b2
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-09-18 17:57:10 +0530

    Add navigation in GlobalNav

--- src/component/FloatingActionButton/FloatingActionButton.tsx
+++ src/component/FloatingActionButton/FloatingActionButton.tsx
@@ -1,15 +1,34 @@
+import {Show} from "solid-js";
 import "./FloatingActionButton.scss";
 
 type FloatingActionButtonProps = {
     icon: string,
     onTap: () => void,
+    selected: boolean,
+    primary?: boolean,
+    image?: string,
     tooltip?: string,
 }
 
 export default function FloatingActionButton(props: FloatingActionButtonProps) {
     return (
-        <div id="FloatingActionButton" onClick={props.onTap} title={props.tooltip}>
-            <i class={props.icon}/>
+        <div
+            id="FloatingActionButton"
+            onClick={props.onTap}
+            title={props.tooltip}
+            style={{
+                "background-color": props.primary ? "orange" : "white",
+                "position": props.selected ? "relative" : "absolute",
+                "box-shadow": props.selected ? 'rgba(0, 0, 0, 0.35) 0 5px 15px' : 'none'
+            }}
+        >
+            <Show
+                when={!props.primary}
+                fallback={<i class={props.icon}/>}
+                keyed
+            >
+                <img src={props.image} alt={props.tooltip}/>
+            </Show>
         </div>
     )
 }
--- src/component/Global-Nav/GlobalNav.tsx
+++ src/component/Global-Nav/GlobalNav.tsx
@@ -2,6 +2,13 @@ import "./GlobalNav.scss";
 import {createSignal, Show} from "solid-js";
 import FloatingActionButton from "../FloatingActionButton/FloatingActionButton";
 import {useNavigate} from "@solidjs/router";
+import cycling from "../../assets/cycling/icon.png";
+import cycleRepairing from "../../assets/cycle-repairing/icon.png";
+import mobileRepairing from "../../assets/mobile-repairing/icon.png";
+import computerRepairing from "../../assets/computer-repairing/icon.png";
+import speakerRepairing from "../../assets/speakers-repairing/icon.png";
+import electronicRepairing from "../../assets/electronics-repairing/icon.png";
+import flyingKites from "../../assets/flying-kites/icon.png";
 
 export default function GlobalNav() {
     const navigate = useNavigate();
@@ -15,6 +22,12 @@ export default function GlobalNav() {
         }
     }
 
+    function link(url: string) {
+        return () => {
+            navigate(url);
+        }
+    }
+
     return (
         <>
             <Show when={selected()} keyed>
@@ -23,7 +36,51 @@ export default function GlobalNav() {
             <div id="GlobalNav">
                 <FloatingActionButton
                     icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/flying-kites")}
+                    selected={selected()}
+                    image={flyingKites}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/electronics-repairing")}
+                    selected={selected()}
+                    image={electronicRepairing}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/speakers-repairing")}
+                    selected={selected()}
+                    image={speakerRepairing}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/computer-repairing")}
+                    selected={selected()}
+                    image={computerRepairing}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/mobile-repairing")}
+                    selected={selected()}
+                    image={mobileRepairing}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/cycle-repairing")}
+                    selected={selected()}
+                    image={cycleRepairing}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
+                    onTap={link("/cycling")}
+                    selected={selected()}
+                    image={cycling}
+                />
+                <FloatingActionButton
+                    icon={selected() ? 'fa-solid fa-house-user' : 'fa-solid fa-bars'}
                     onTap={onMenuClick}
+                    selected={selected()}
+                    primary={true}
                 />
             </div>
         </>
