312de7650028c15f9f1f1b535595a7aba426393e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-09-18 08:54:59 +0530

    Create image download button

--- src/component/ImageViwer/ImageViewer.tsx
+++ src/component/ImageViwer/ImageViewer.tsx
@@ -17,11 +17,25 @@ export default function ImageViewer() {
         navigate(`/${location.state.imageLocation}`);
     }
 
+    async function onDownloadButtonClick() {
+        const image = await fetch(location.state.imageUrl);
+        const imageBlog = await image.blob();
+        const imageURL = URL.createObjectURL(imageBlog);
+
+        const link = document.createElement('a');
+        link.href = imageURL;
+        link.download = location.state.imageName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return (
         <div id="ImageViewer">
             <img src={location.state.imageUrl} alt={location.state.imageName}/>
             <header>
                 <IconButton iconClass="fa-solid fa-arrow-left" onClick={onBackButtonClick}/>
+                <IconButton iconClass="fa-solid fa-download" onClick={onDownloadButtonClick}/>
             </header>
         </div>
     )
