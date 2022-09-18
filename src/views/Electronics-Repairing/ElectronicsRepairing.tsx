13b81c6bf257fa1fd8a20a67bc04b35479879dcf
Shuktika Mahanty <shuktika.mahanty@gmail.com>
2022-09-18 14:38:02 +0530

    Accept incoming changes

--- src/views/Electronics-Repairing/ElectronicsRepairing.tsx
+++ src/views/Electronics-Repairing/ElectronicsRepairing.tsx
@@ -5,11 +5,7 @@ import headerImage from "../../assets/electronics-repairing/header.gif";
 export default function ElectronicsRepairing() {
     return (
         <>
-<<<<<<< HEAD
-            <Header title="Cycling" img={headerImage}/>
-=======
             <Header title="Electronics Repairing" position="top" img={headerImage}/>
->>>>>>> 0402277c17a76984a3b7dfa232d803df5138d79d
             <div class="container">
                 <div class="description">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae consequuntur deleniti ducimus
