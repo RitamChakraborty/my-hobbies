0b18363ebbb45a2be94cd0063b6c2f71ecb5f5a9
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-09-18 09:50:20 +0530

    Create hobby list

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,20 +1,23 @@
 import Header from "../../component/Header/Header";
 import homeHeader from "../../assets/home/home.gif";
 import {Link} from "@solidjs/router";
+import "./Home.scss";
 
 export default function Home() {
     return (
-        <>
+        <div id="Home">
             <Header title="A List of My Hobbies" img={homeHeader}/>
             <div class="container">
                 <nav>
-                    <ul>
-                        <li>
-                            <Link href={"/cycling"}>Cycling</Link>
-                        </li>
-                    </ul>
+                    <Link href={"/cycling"}>Cycling</Link>
+                    <Link href={"/cycle-repairing"}>Cycle Repairing</Link>
+                    <Link href={"/mobile-repairing"}>Mobile Repairing</Link>
+                    <Link href={"/computer-repairing"}>Computer and Laptop Repairing</Link>
+                    <Link href={"/speaker-repairing"}>Speaker and Headphone Repairing</Link>
+                    <Link href={"/cycle-repairing"}>Cycle Repairing</Link>
+                    <Link href={"/flying-kites"}>Flying Kites</Link>
                 </nav>
             </div>
-        </>
+        </div>
     );
 }
