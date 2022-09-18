c1d7f8cd78f773f04b09ec300eff42619e42b96b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-09-18 07:27:48 +0530

    Add delay before displaying the images

--- src/component/Image-Gallery/ImageGallery.tsx
+++ src/component/Image-Gallery/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import {For} from "solid-js";
+import {createSignal, For, onMount} from "solid-js";
 import "./ImageGallery.scss";
 import Image from "../Image/Image";
 
@@ -9,18 +9,28 @@ type ImageGalleryProps = {
 }
 
 export default function ImageGallery(props: ImageGalleryProps) {
-    function images(): string[] {
-        const images = [];
+    const [images, setImages] = createSignal<string[]>([]);
+
+    function pause(delay: number) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('resolve');
+            }, delay);
+        });
+    }
+
+    onMount(async () => {
+        await pause(600);
 
         for (let i = 0; i < props.imagesCount; ++i) {
-            images.push(new URL(
+            const url = new URL(
                 `../../assets/${props.imagesLocation}/images/${i + 1}.${props.imagesExtension}`,
-                import.meta.url).href
-            );
+                import.meta.url
+            ).href;
+            await pause(100);
+            setImages((value) => [...value, url]);
         }
-
-        return images;
-    }
+    })
 
     return (
         <div id="ImageGallery">
